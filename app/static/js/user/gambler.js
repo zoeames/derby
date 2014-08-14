1f3fa4bb5f6119e647409a11e215ba619b1a86d3
Zoe Ames <zoeames@gmail.com>
2014-08-14 14:31:50 -0500

    sell asset ajax

---
+++ app/static/js/user/gambler.js
@@ -0,0 +1,16 @@
+(function(){
+  'use strict';
+
+  $(document).ready(function(){
+    $('.info').click(sellAsset)
+  });
+
+  function sellAsset(){
+    //debugger;
+    var id   = $(this).closest('.gambler').attr('data-gambler-id');
+    var item   = $(this).children('.name').text();
+    console.log(id, item);
+  }
+
+})();
+
